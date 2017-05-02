62b502ea1dc7c186c069c1057b46ea10afeb9e11
Brian Rush and Corey Redding <brian+corey@>
2017-05-02 12:45:39 -0700

    add more edits to ef else branching in javascript

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
   $("form").submit(function(event) {
     event.preventDefault();
-
+    $('.output').empty();
     //Assign input 1 to variable
     var countToInput = parseFloat($("#count-to").val());
 
@@ -21,8 +21,20 @@ $(document).ready(function() {
     } else if (countToInput < countByInput) {
       $("#error-1").text("This number should be bigger than your other number, idiot.");
       $("#hidden-label-1").show();
-
+    } else {
+      $("#hidden-label-1").hide();
     };
+
+    if (isNaN(countByInput)) {
+      $("#error-2").text("Please enter a number.");
+      $("#hidden-label-2").show();
+    } else if (countByInput <= 0) {
+      $("#error-2").text("Please enter a positive number.");
+      $("#hidden-label-2").show();
+    } else {
+      $("#hidden-label-1").hide();
+    };
+
 
 
     for (var index = countByInput; index <= countToInput; index += countByInput) {
